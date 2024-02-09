7310db494d942f8239a724137497ea5926311f1a
Dartrix137 <daniel.ftg@hotmail.com>
2024-02-09 16:30:08 -0500

    deploying

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -4,7 +4,7 @@ function AboutPage() {
   return (
     <section className="container mx-auto">
       <h3 className="text-4xl font-bold my-5">About</h3>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias consequatur eveniet quod earum, iusto fugit laudantium rem blanditiis ipsam laborum odit tenetur perferendis sit eaque deleniti facilis excepturi delectus dolorem nemo quos consectetur. Ut quae eos necessitatibus enim ab magni amet officiis mollitia aspernatur tempore reiciendis repudiandae quasi officia animi harum, possimus rerum earum vel quidem exercitationem dolorem error, delectus quas atque? Assumenda quam sint quibusdam hic quod illo, amet explicabo veniam repellendus. Fugit ab temporibus laboriosam, excepturi unde quasi autem! Autem nulla numquam, neque, hic, eveniet voluptatibus mollitia eius totam tenetur quisquam nemo non accusamus! Voluptate, nihil nostrum. Facere.</p>
+      <p className='text-2xl text-justify'>The developed project is a CRUD (Create, Read, Update, Delete) task management system implemented using Next.js as the React framework, PostgreSQL as the relational database, and Prisma ORM to facilitate database operations. This system allows users to manage their tasks efficiently and organized.</p>
     </section>
   )
 }
